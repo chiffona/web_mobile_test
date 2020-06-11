078055fef9a13c753d8d06664ccfe466ec28ed30
chiffona <66194623+chiffona@users.noreply.github.com>
2020-06-11 21:20:04 +0900

    123

---
+++ js/us/button_con.js
@@ -0,0 +1,12 @@
+    // html dom 이 다 로딩된 후 실행된다.
+    $(document).ready(function(){
+        // memu 클래스 바로 하위에 있는 a 태그를 클릭했을때
+        $(".menu>a").click(function(){
+            // 현재 클릭한 태그가 a 이기 때문에
+            // a 옆의 태그중 ul 태그에 hide 클래스 태그를 넣던지 빼던지 한다.
+            $(this).next("ul").toggleClass("hide");
+        });
+    });
+
+
+출처: https://stove99.tistory.com/103 [스토브 훌로구]
